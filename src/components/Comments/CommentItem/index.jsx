006a132afd2ae9db0b7d70bfdffdbee7b710e7b6
vladiva! <105559294+vladivap@users.noreply.github.com>
2023-02-04 16:11:02 +0300

    Add files via upload

--- src/components/Comments/CommentItem/index.jsx
+++ src/components/Comments/CommentItem/index.jsx
@@ -1,11 +1,15 @@
-import './styles.css'
+import "./styles.css";
 
 export const CommentItem = ({ name, date, text }) => {
-	return <div className="comment-item">
-		<div className="comment-header">
-			<b>{name}</b>
-			<span className="comment-date">{date}</span>
-		</div>
-		{text}
-	</div>
-}
+	console.log(name, date, text);
+	
+  return (
+    <div className="comment-item">
+      <div className="comment-header">
+        <b>{name}</b>
+        <span className="comment-date">{date}</span>
+      </div>
+      {text}
+    </div>
+  );
+};
